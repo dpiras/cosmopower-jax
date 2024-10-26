cc0989278ac4f5da75bc72a00f4ccb478525e25e
Davide Piras <dr.davide.piras@gmail.com>
2024-10-26 15:00:55 +0200

    Update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ PACKAGENAME = 'cosmopower_jax'
 
 setup(
     name='cosmopower_jax',
-    version="0.5.3",
+    version="0.5.4",
     author='Davide Piras',
     author_email='davide.piras@unige.ch',
     description='Differentiable cosmological emulators',
